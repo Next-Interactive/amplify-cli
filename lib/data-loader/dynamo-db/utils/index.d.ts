9d2c35747e6b4849e0801fbd1ae908c642083e3f
Valentin GIRAUD <giraud.valentin@gmail.com>
2022-07-20 16:47:49 +0200

    set code to original packages/amplify-appsync-simulator of amplify-cli of 2.4.0 (2022-06-03)

--- lib/data-loader/dynamo-db/utils/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function nullIfEmpty(obj: object): object | null;
-export declare function unmarshall(raw: any, isRaw?: boolean): any;
-declare type TableObject<T> = {
-    [tableName: string]: T;
-};
-export declare function mapTableObject<T, R>(tableObject: TableObject<T>, mapper: (inp: T) => R): TableObject<R>;
-export {};
--- lib/velocity/util/math.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const math: {
-    roundNum: (x: number) => number;
-    minVal: (...values: number[]) => number;
-    maxVal: (...values: number[]) => number;
-    randomDouble: () => number;
-    randomWithinRange: any;
-};
